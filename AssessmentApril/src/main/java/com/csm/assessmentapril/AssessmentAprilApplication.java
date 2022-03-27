aa8a8656b0398819d0065a27fa1260db849c8361
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-03-27 23:30:24 +0530

    commit

---
+++ AssessmentApril/src/main/java/com/csm/assessmentapril/AssessmentAprilApplication.java
@@ -0,0 +1,13 @@
+package com.csm.assessmentapril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentAprilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentAprilApplication.class, args);
+    }
+
+}
---
+++ AssessmentApril/src/test/java/com/csm/assessmentapril/AssessmentAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.assessmentapril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentAprilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
