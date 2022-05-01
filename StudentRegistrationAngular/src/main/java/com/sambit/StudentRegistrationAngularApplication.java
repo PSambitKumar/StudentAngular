873af3f8d40ca50943f97ae900333456c36b02a9
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-05-02 03:37:19 +0530

    First Commit

---
+++ StudentRegistrationAngular/src/main/java/com/sambit/StudentRegistrationAngularApplication.java
@@ -0,0 +1,13 @@
+package com.sambit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegistrationAngularApplication.class, args);
+    }
+
+}
