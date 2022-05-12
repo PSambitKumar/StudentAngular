6779affbfbdb8778f9e552cf4d560ed8b6c97d8a
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-05-12 21:33:52 +0530

    Farmer Registration using Unique ID Farmer Family add by OneToMany Mapping Save Farmer with Family into Database.

---
+++ ProductMasterSpringboot/src/main/java/com/csm/productmasterspringboot/ProductMasterSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.csm.productmasterspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMasterSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMasterSpringbootApplication.class, args);
+    }
+
+}
---
+++ ProductMasterSpringboot/src/test/java/com/csm/productmasterspringboot/ProductMasterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.productmasterspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMasterSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
