76c946abff53e9b955c7ce587be1c9e581545e8d
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-06-23 23:18:54 +0530

    User Creation New Assignment Repository Added

---
+++ UserCreation/src/main/java/com/csm/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.csm.Service;
+
+public interface UserService {
+}
---
+++ UserCreation/src/main/java/com/csm/ServiceImpl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.csm.ServiceImpl;
+
+import com.csm.Service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
