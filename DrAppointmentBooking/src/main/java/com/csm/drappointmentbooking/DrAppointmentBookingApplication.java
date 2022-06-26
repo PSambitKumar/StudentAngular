dfa748bf67eaa5ca535c855477b9f0b88167608f
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-06-26 21:53:21 +0530

    Appointment

---
+++ DrAppointmentBooking/src/main/java/com/csm/drappointmentbooking/DrAppointmentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.csm.drappointmentbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrAppointmentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrAppointmentBookingApplication.class, args);
+	}
+
+}
---
+++ DrAppointmentBooking/src/test/java/com/csm/drappointmentbooking/DrAppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.drappointmentbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrAppointmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
