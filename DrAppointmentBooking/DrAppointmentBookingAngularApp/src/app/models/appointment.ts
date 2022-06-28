9245c6164257729d2027e095b07bd1ccd69c755e
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-06-28 14:48:25 +0530

    Appointment Angular Added

---
+++ DrAppointmentBooking/DrAppointmentBookingAngularApp/src/app/models/appointment.ts
@@ -0,0 +1,11 @@
+export class Appointment{
+  id : any;
+  name:  any;
+  age:  any;
+  gender:  any;
+  apptDate:  any;
+  email:  any;
+  descr:  any;
+  address:  any;
+  mobile:  any;
+}
---
+++ DrAppointmentBooking/src/main/java/com/csm/drappointmentbooking/Controller/AppointmentAngularController.java
@@ -0,0 +1,12 @@
+package com.csm.drappointmentbooking.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(value = "/appointment/v1")
+public class AppointmentAngularController {
+
+}
